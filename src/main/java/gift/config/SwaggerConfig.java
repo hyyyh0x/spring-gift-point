fc45eae556db7274393750cfc9b76bf482ea8343
hyyyh0x <hyyyh0x@gmail.com>
2024-07-31 17:23:22 +0900

    log

--- src/main/java/gift/config/SwaggerConfig.java
+++ src/main/java/gift/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,7 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI(){
         String securityJwtName = "JWT";
         return new OpenAPI()
+            .addServersItem(new Server().url("/"))
             .addSecurityItem(new SecurityRequirement().addList(securityJwtName))
             .components(new Components()
                 .addSecuritySchemes(securityJwtName, new SecurityScheme()
