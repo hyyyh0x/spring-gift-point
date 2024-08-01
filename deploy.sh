0805ea07d952780987f956e2cc0b33d2c7063e35
hyyyh0x <hyyyh0x@gmail.com>
2024-08-01 14:51:19 +0900

    feat: cors, deploy

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+GITHUB_ID = "hyyyh0x"
+APP_NAME = "spring-gift"
+PROJECT_NAME = "spring-gift-point"
+PROJECT_VERSION = "0.0.1"
+REPO_URL = "https://github.com/${GITHUB_ID}/${PROJECT_NAME}"
+BRANCH = "step1_1"
+DIR="/home/ubuntu"
+JAR_FILE="${APP_NAME}-${PROJECT_VERSION}-SNAPSHOT.jar"
+
+port=$(lsof -i :8080 -t)
+if [ -n "$port" ]; then
+    kill -9 $port
+fi
+
+cd ${DIR}
+
+if [ ! -d "${PROJECT_NAME}" ]; then
+    git clone -b ${BRANCH} --single-branch ${REPO_URL}
+else
+    cd ${PROJECT_NAME}
+    git pull origin ${BRANCH}
+fi
+
+cd ${DIR}/${PROJECT_NAME}
+./gradlew bootJar
+cd build/libs
+nohup java -jar ${JAR_FILE} &
---
+++ src/main/java/gift/filter/CorsFilter.java
@@ -0,0 +1,42 @@
+package gift.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+        FilterChain filterChain)
+        throws ServletException, IOException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
