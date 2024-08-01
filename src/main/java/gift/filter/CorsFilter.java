8bb496957a2f79b86a044b98efccef32f152c942
hyyyh0x <hyyyh0x@gmail.com>
2024-08-01 16:17:39 +0900

    refactor: CorsFilter

--- src/main/java/gift/filter/CorsFilter.java
+++ src/main/java/gift/filter/CorsFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import org.springframework.core.Ordered;
@@ -26,12 +27,18 @@ public class CorsFilter implements Filter {
         FilterChain filterChain)
         throws ServletException, IOException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
 
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Allow-Headers", "*");
 
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
